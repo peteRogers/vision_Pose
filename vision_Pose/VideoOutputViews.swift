b085c73c289217a1ba121eaf761d35b10efd24ab
Peter Rogers <p.rogers@gold.ac.uk>
2021-11-10 10:46:28 +0000

    first commit

---
+++ vision_Pose/VideoOutputViews.swift
@@ -0,0 +1,47 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+Custom views that are presenting video from different sources:
+     CameraFeedView - displays video frames captured by the camera.
+     VideoRenderView - displays video frames read from the prerecorded file.
+*/
+
+import UIKit
+import AVFoundation
+
+// MARK: - Coordinates conversion
+protocol NormalizedGeometryConverting {
+    func viewRectConverted(fromNormalizedContentsRect normalizedRect: CGRect) -> CGRect
+    func viewPointConverted(fromNormalizedContentsPoint normalizedPoint: CGPoint) -> CGPoint
+}
+
+// MARK: - View to display live camera feed
+class CameraFeedView: UIView, NormalizedGeometryConverting {
+    private var previewLayer: AVCaptureVideoPreviewLayer!
+    
+    override class var layerClass: AnyClass {
+        return AVCaptureVideoPreviewLayer.self
+    }
+    
+    init(frame: CGRect, session: AVCaptureSession, videoOrientation: AVCaptureVideoOrientation) {
+        super.init(frame: frame)
+        previewLayer = layer as? AVCaptureVideoPreviewLayer
+        previewLayer.session = session
+        previewLayer.videoGravity = .resizeAspect
+        previewLayer.connection?.videoOrientation = videoOrientation
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func viewRectConverted(fromNormalizedContentsRect normalizedRect: CGRect) -> CGRect {
+        return previewLayer.layerRectConverted(fromMetadataOutputRect: normalizedRect)
+    }
+
+    func viewPointConverted(fromNormalizedContentsPoint normalizedPoint: CGPoint) -> CGPoint {
+        return previewLayer.layerPointConverted(fromCaptureDevicePoint: normalizedPoint)
+    }
+}
+
--- vision_Pose/ViewController.swift
+++ vision_Pose/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    private var cameraViewController: CameraViewController!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        cameraViewController = CameraViewController()
+        cameraViewController.view.frame = view.bounds
+        addChild(cameraViewController)
+        cameraViewController.beginAppearanceTransition(true, animated: true)
+        view.addSubview(cameraViewController.view)
+        cameraViewController.endAppearanceTransition()
+        cameraViewController.didMove(toParent: self)
         // Do any additional setup after loading the view.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        do {
+       
+            try cameraViewController.setupAVSession()
+       
+                   } catch {
+                      // AppError.display(error, inViewController: self)
+                   }
+    }
 
 }
 
